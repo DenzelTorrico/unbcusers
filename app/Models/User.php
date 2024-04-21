9613cefa7bde09719ec156957ba4b418904e3ffc
DenzelTorrico <denzel_silva@hotmail.com>
2024-04-21 18:32:37 -0500

    refactor y reglas

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,37 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function rules($editPassword)
+    {
+        $rules = [
+            'name' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|regex:/(.+)@(.+)\.(.+)/i',
+            'phone' => 'required|min:9|max:9',
+        ];
+
+        // Si no se proporciona una contraseña, se excluye la validación de la misma
+        if (!$editPassword) {
+            $rules['password'] = 'required|min:8';
+        }
+
+        return $rules;
+    }
+
+    public function customMessages()
+    {
+        return [
+            'name.required' => 'El campo nombre es obligatorio.',
+            'lastname.required' => 'El campo apellido es obligatorio.',
+            'email.required' => 'El campo correo electrónico es obligatorio.',
+            'email.email' => 'El correo electrónico debe ser una dirección de correo válida.',
+            'email.regex' => 'Tiene que tener un formato correcto de correo',
+            'phone.required' => 'El campo teléfono es obligatorio.',
+            'phone.min' => 'El campo teléfono debe tener al menos :min dígitos.',
+            'phone.max' => 'El campo teléfono debe tener como máximo :max dígitos.',
+            'password.required' => 'El campo contraseña es obligatorio.',
+            'password.min' => 'La contraseña debe tener al menos :min caracteres.',
+        ];
+    }
 }
